a987dd488cee5c63bdd74cd2df795b1a6dea7686
James Folk <james@njligames.com>
2020-05-19 00:45:36 -0400

    test

--- NJLICColorTest.h
+++ NJLICColorTest.h
@@ -9,7 +9,7 @@
 #define NJLICColorTest_h
 
 #include "Color.h"
-#include "NJLICGame.h"
+#include <thirdparty/includes/NJLICEngine/NJLICGame.h>
 
 class NJLICColorTest : public NJLICGame {
     Color mColor;
