9686d2dfa465ca374b16f139c9cc4c892eca6d13
James Folk <james@njligames.com>
2020-05-22 11:20:57 -0400

    you can now choose what opengl platform to use

--- src/platform/macos/public_include/GraphicsPlatform.h
+++ src/platform/macos/public_include/GraphicsPlatform.h
@@ -1,27 +1,34 @@
 
-//#if defined(__GL_ES2__)
-//  #error Unsupported choice setting
-//#elif defined(__GL_ES3__)
-//  #error Unsupported choice setting
-//#elif defined(__GL_2__)
+#if defined(__GL_ES2__)
 
-#if defined(__APPLE__)
-#define glGenVertexArrays_NJLIC glGenVertexArraysAPPLE
-#define glBindVertexArray_NJLIC glBindVertexArrayAPPLE
-#define glDeleteVertexArrays_NJLIC glDeleteVertexArraysAPPLE
+    #error Unsupported choice setting
+
+#elif defined(__GL_ES3__)
+
+    #error Unsupported choice setting
+
+#elif defined(__GL_2__)
+
+    #if defined(__APPLE__)
+        #define glGenVertexArrays_NJLIC glGenVertexArraysAPPLE
+        #define glBindVertexArray_NJLIC glBindVertexArrayAPPLE
+        #define glDeleteVertexArrays_NJLIC glDeleteVertexArraysAPPLE
+    #else
+        #define glGenVertexArrays_NJLIC glGenVertexArrays
+        #define glBindVertexArray_NJLIC glBindVertexArray
+        #define glDeleteVertexArrays_NJLIC glDeleteVertexArrays
+    #endif
+
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glext.h>
+
+#elif defined(__GL_3__)
+
+    #include <OpenGL/gl3.h>
+    #include <OpenGL/gl3ext.h>
+
 #else
-#define glGenVertexArrays_NJLIC glGenVertexArrays
-#define glBindVertexArray_NJLIC glBindVertexArray
-#define glDeleteVertexArrays_NJLIC glDeleteVertexArrays
+
+    #error Unsupported choice setting
+
 #endif
-
-#include <OpenGL/gl.h>
-#include <OpenGL/glext.h>
-
-//#elif defined(__GL_3__)
-//  #include <OpenGL/gl3.h>
-//  #include <OpenGL/gl3ext.h>
-//  #define NANOVG_GL3_IMPLEMENTATION
-//#else
-//  #error Unsupported choice setting
-//#endif
