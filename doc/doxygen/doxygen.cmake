fdc490c1ed4e6ecb6fcbdf79fda01982e51154da
James Folk <james@njligames.com>
2020-05-21 23:15:06 -0400

    added install for doxygen and swig

--- doc/doxygen/doxygen.cmake
+++ doc/doxygen/doxygen.cmake
@@ -2,13 +2,18 @@
 option(BUILD_DOC "Build ${PROJECT_NAME} documentation" ON)
 
 # check if Doxygen is installed
-find_package(Doxygen)
+find_package(Doxygen
+    REQUIRED dot
+    OPTIONAL_COMPONENTS mscgen dia
+    )
 if (DOXYGEN_FOUND)
+    message(STATUS "DOXYGEN_FOUND ${DOXYGEN_FOUND}")
+    message(STATUS "DOXYGEN_VERSION ${DOXYGEN_VERSION}")
     # set input and output files
     set(DOXYGEN_DIR ${CMAKE_CURRENT_SOURCE_DIR})
 
     set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
-    set(DOXYGEN_OUT ${CMAKE_BINARY_DIR}/Doxyfile)
+    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
     # request to configure the file
     configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
@@ -16,19 +21,20 @@ if (DOXYGEN_FOUND)
 
     # note the option ALL which allows to build the docs together with the application
     add_custom_target( ${PROJECT_NAME}-documentation ALL
-        SOURCES ${CMAKE_BINARY_DIR}/Doxyfile
+        SOURCES ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
         COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         COMMENT "Generating API documentation with Doxygen"
         VERBATIM
         )
     
-    file(GLOB_RECURSE TARGET_GENERATED_FILES "${CMAKE_BINARY_DIR}/doc/html/*.*")
+    file(GLOB_RECURSE TARGET_GENERATED_FILES "${CMAKE_CURRENT_BINARY_DIR}/../html/*.*")
 
-    set_property(DIRECTORY PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${TARGET_GENERATED_FILES})
-    set_source_files_properties( ${TARGET_GENERATED_FILES} PROPERTIES GENERATED TRUE )
+    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../html DESTINATION share/doc)
 
-    # INSTALL(DIRECTORY ${CMAKE_BINARY_DIR}/doc/html DESTINATION ${CMAKE_BINARY_DIR}/doc)
+    foreach(file ${TARGET_GENERATED_FILES})
+        set_source_files_properties( ${file} PROPERTIES GENERATED TRUE )
+    endforeach()
 
 else (DOXYGEN_FOUND)
     message("Doxygen need to be installed to generate the doxygen documentation")
