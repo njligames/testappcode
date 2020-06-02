bd8605d975fa20fae04ff58dddb624568da7a0f1
James Folk <jamesfolk1@gmail.com>
2020-06-02 14:08:17 -0400

    added linux

---
+++ src/platform/linux/public_include/GraphicsPlatform.h
@@ -0,0 +1,15 @@
+
+#if defined(__GL_ES2__)
+    #error Unsupported choice setting
+#elif defined(__GL_ES3__)
+    #error Unsupported choice setting
+#elif defined(__GL_2__)
+    #define GL_GLEXT_PROTOTYPES
+    #include <GL/gl.h>
+    #include <GL/glext.h>
+#elif defined(__GL_3__)
+    #include <OpenGL/gl3.h>
+    #include <OpenGL/gl3ext.h>
+#else
+    #error Unsupported choice setting
+#endif
